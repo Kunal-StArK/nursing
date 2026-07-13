4a60cc6e3d2bddd7d52716a54eaa7af3c080b09f
Kunal-StArK <kunal73014@gmail.com>
2026-07-13 17:19:13 +0530

    added img for doctors and stats

--- doctors/models.py
+++ doctors/models.py
@@ -6,4 +6,6 @@ class Doctors(models.Model):
     doctor_name =models.CharField(max_length=30)
     doctor_specialist= models.CharField(max_length=20)
     doctor_experience = models.TextField()
-    doctor_img = models.ImageField(upload_to=)
+    doctor_img = models.ImageField(upload_to='uploads', null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
