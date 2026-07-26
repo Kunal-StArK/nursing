a58ee1f8af087ef68b1ffebfb58dbb4b406125e7
Kunal-StArK <kunal73014@gmail.com>
2026-07-26 12:39:46 +0530

    mail

--- Hopital/settings.py
+++ Hopital/settings.py
@@ -172,8 +172,9 @@ MESSAGE_TAGS = {
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env.int('EMAIL_PORT')  # Casts the port string to an integer
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Casts the port string to an integer
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
